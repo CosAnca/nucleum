776be8ff9258157642b58a79a1df4fc26cf4b744
Cos <cosmin.anca@yahoo.com>
2018-03-10 09:04:35 +0000

    Fix bin script module resolving

--- bin/fosterkit.js
+++ bin/fosterkit.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
+const path = require('path');
 const additionalArgs = require('minimist')(process.argv.slice(2))._;
+const fosterkitEntryFile = require.resolve('fosterkit');
+const gulpModulePath = path.dirname(require.resolve('gulp'));
+const gulpBinaryFile = path.join(gulpModulePath, '/bin/gulp');
 
-let args = ['--gulpfile', 'node_modules/fosterkit/gulpfile.js'];
+let args = ['--gulpfile', fosterkitEntryFile];
 
 if (additionalArgs.length) {
   args = args.concat(additionalArgs);
 }
 
-require('child_process').fork('node_modules/gulp/bin/gulp', args);
+require('child_process').fork(gulpBinaryFile, args);
