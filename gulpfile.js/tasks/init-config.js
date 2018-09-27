3c13b66913f0d222606918565239ba0dcdf682a9
Cos <cos@anca.im>
2018-09-27 11:39:22 +0100

    Add postcss with presetEnv, normalize and purgecss plugins

--- gulpfile.js/tasks/init-config.js
+++ gulpfile.js/tasks/init-config.js
@@ -1,16 +1,16 @@
-const gulp = require('gulp');
-const log = require('fancy-log');
-const colors = require('ansi-colors');
-const projectPath = require('../lib/projectPath');
+const gulp = require("gulp");
+const log = require("fancy-log");
+const colors = require("ansi-colors");
+const projectPath = require("../lib/projectPath");
 
-gulp.task('init-config', function() {
+gulp.task("init-config", function() {
   const configStream = gulp
-    .src(['gulpfile.js/path-config.json', 'gulpfile.js/task-config.js'])
-    .pipe(gulp.dest(projectPath('config')));
+    .src(["gulpfile.js/path-config.json", "gulpfile.js/task-config.js"])
+    .pipe(gulp.dest(projectPath("config")));
 
   log(
     colors.green(
-      'Adding default path-config.json and task-config.js files to ./config/'
+      "Adding default path-config.json and task-config.js files to ./config/"
     )
   );
 
