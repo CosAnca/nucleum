72b4ac2fa65c97db6afaec121baaf5b1d4c7d69d
Cos <cos@anca.im>
2020-05-16 18:32:12 +0100

    Add .eleventy files into dotfiles dir

---
+++ extras/dotfiles/.eleventy.js
@@ -0,0 +1,28 @@
+module.exports = function (eleventyConfig) {
+  // Filters
+  eleventyConfig.addNunjucksFilter("absoluteUrl", (href, base) => {
+    let { URL } = require("url");
+    return new URL(href, base).toString();
+  });
+
+  eleventyConfig.addNunjucksFilter("dateFormatISO", (value) => {
+    const date = new Date(value);
+    return date.toISOString().slice(0, 10);
+  });
+
+  // Layouts
+  eleventyConfig.addLayoutAlias("base", "_base.njk");
+
+  return {
+    dir: {
+      input: "src/views",
+      data: "data",
+      includes: "includes",
+      layouts: "layouts",
+      output: "public",
+    },
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
