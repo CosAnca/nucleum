4f551d07591f05c0e1b47a4644412534dcce990b
Cos <cos@anca.im>
2021-09-10 12:38:51 +0100

    Move styles architecture to Sass Modules and drop IE support

--- gulpfile.js/lib/config-defaults.js
+++ gulpfile.js/lib/config-defaults.js
@@ -25,6 +25,7 @@ module.exports = {
       plugins: [
         ["@babel/plugin-proposal-class-properties", { loose: true }],
         ["@babel/plugin-proposal-private-methods", { loose: true }],
+        ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
         "@babel/plugin-syntax-dynamic-import",
         "@babel/plugin-transform-runtime",
       ],
